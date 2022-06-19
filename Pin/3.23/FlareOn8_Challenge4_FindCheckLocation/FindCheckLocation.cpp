f450f80741d7091c23b76a3bacb50bc50b4c6c43
ayoubismaili <ayoubismaili1@gmail.com>
2022-06-19 01:33:50 +0100

    Add Image Id, Image Name, Image Base, IP RVA to the details.

--- Pin/3.23/FlareOn8_Challenge4_FindCheckLocation/FindCheckLocation.cpp
+++ Pin/3.23/FlareOn8_Challenge4_FindCheckLocation/FindCheckLocation.cpp
@@ -12,14 +12,17 @@ const char WANTED_STRING[] = "you chose.. poorly";
 
 // This function is called before every instruction is executed
 // and prints the IP
-VOID printip(ADDRINT eip, ADDRINT* ptr) 
+VOID printip(ADDRINT eip, ADDRINT* ptr, UINT32 img_id) 
 { 
     UINT8 value[sizeof(WANTED_STRING)];
+    IMG img = IMG_FindImgById(img_id);
     memset(value, 0, sizeof(value));
     PIN_SafeCopy(value, ptr, sizeof(value) - 1);
     if (strcmp((char*)value, WANTED_STRING) == 0)
     {
-        fprintf(trace, "%p -> %s\n", eip, value);
+        ADDRINT base = IMG_LowAddress(img);
+        ADDRINT rva = eip - base;
+        fprintf(trace, "img_id=%d -> img_name=%s -> base=%p -> rva=%p -> ptr=%p -> %s\n", img_id, IMG_Name(img).c_str(), base, rva, ptr, value);
         fflush(trace);
     }
 }
@@ -30,7 +33,8 @@ VOID Instruction(INS ins, VOID* v)
     // Insert a call to printip before every instruction, and pass it the IP if the ins is mov from memory to register
     if (INS_Opcode(ins) == XED_ICLASS_MOV && INS_IsMemoryRead(ins) && INS_OperandIsReg(ins, 0) && INS_OperandIsMemory(ins, 1))
     {
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)printip, IARG_INST_PTR, IARG_MEMORYREAD_EA, IARG_END);
+        IMG img = IMG_FindByAddress(INS_Address(ins));
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)printip, IARG_INST_PTR, IARG_MEMORYREAD_EA, IARG_UINT32, IMG_Id(img), IARG_END);
     }
 }
 
