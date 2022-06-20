b7e9812cdcb9a8c842f6de8460a8b24685a8e587
ayoubismaili <ayoubismaili1@gmail.com>
2022-06-20 02:04:40 +0100

    Add few functions.

--- Pin/3.23/RtnNameTracing/RtnNameTracing.cpp
+++ Pin/3.23/RtnNameTracing/RtnNameTracing.cpp
@@ -15,7 +15,7 @@ VOID Routine_CreateFileA(ADDRINT ip, ADDRINT sp)
 {
     ADDRINT value[8];
     CHAR lpFileName[50];
-    const CHAR *args[] = {"retval", "lpFileName", "dwDesiredAccess", "dwShareMode",
+    const CHAR *args[] = {"retAddress", "lpFileName", "dwDesiredAccess", "dwShareMode",
         "lpSecurityAttributes", "dwCreationDisposition", "dwFlagsAndAttributes", "hTemplateFile"};
     std::string rtnName = RTN_FindNameByAddress(ip);
     //Zero Memory
@@ -45,7 +45,7 @@ VOID Routine_LoadLibraryA(ADDRINT ip, ADDRINT sp)
 {
     ADDRINT value[2];
     CHAR lpLibFileName[50];
-    const CHAR* args[] = { "retval", "lpLibFileName" };
+    const CHAR* args[] = { "retAddress", "lpLibFileName" };
     std::string rtnName = RTN_FindNameByAddress(ip);
     //Zero Memory
     memset(value, 0, sizeof(value));
@@ -66,6 +66,125 @@ VOID Routine_LoadLibraryA(ADDRINT ip, ADDRINT sp)
             {
                 fprintf(traceF, "  sp[%d]=%p (%s)\n", i, value[i], args[i]);
             }
+        }
+    }
+}
+
+VOID Routine_lstrlenA(ADDRINT ip, ADDRINT sp)
+{
+    ADDRINT value[2];
+    CHAR lpString[50];
+    const CHAR* args[] = { "retAddress", "lpString" };
+    std::string rtnName = RTN_FindNameByAddress(ip);
+    //Zero Memory
+    memset(value, 0, sizeof(value));
+    memset(lpString, 0, sizeof(lpString));
+    //Read value pointed by SP
+    PIN_SafeCopy(value, (VOID*)sp, sizeof(value));
+    PIN_SafeCopy(lpString, (VOID*)value[1], sizeof(lpString) - 1);
+    if (!rtnName.empty())
+    {
+        fprintf(traceF, "ip=%p, rtn_name=%s, sp=%p\n", ip, rtnName.c_str(), sp);
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 1) //lpString
+            {
+                fprintf(traceF, "  sp[%d]=%p (%s) = %s\n", i, value[i], args[i], lpString);
+            }
+            else
+            {
+                fprintf(traceF, "  sp[%d]=%p (%s)\n", i, value[i], args[i]);
+            }
+        }
+    }
+}
+
+VOID Routine_RegOpenKeyExA(ADDRINT ip, ADDRINT sp)
+{
+    ADDRINT value[6];
+    CHAR lpSubKey[50];
+    const CHAR* args[] = { "retAddress", "hKey", "lpSubKey", "ulOptions", "samDesired", "phkResult"};
+    std::string rtnName = RTN_FindNameByAddress(ip);
+    //Zero Memory
+    memset(value, 0, sizeof(value));
+    memset(lpSubKey, 0, sizeof(lpSubKey));
+    //Read value pointed by SP
+    PIN_SafeCopy(value, (VOID*)sp, sizeof(value));
+    PIN_SafeCopy(lpSubKey, (VOID*)value[2], sizeof(lpSubKey) - 1);
+    if (!rtnName.empty())
+    {
+        fprintf(traceF, "ip=%p, rtn_name=%s, sp=%p\n", ip, rtnName.c_str(), sp);
+        for (int i = 0; i < 6; i++)
+        {
+            if (i == 2) //lpSubKey
+            {
+                fprintf(traceF, "  sp[%d]=%p (%s) = %s\n", i, value[i], args[i], lpSubKey);
+            }
+            else
+            {
+                fprintf(traceF, "  sp[%d]=%p (%s)\n", i, value[i], args[i]);
+            }
+        }
+    }
+}
+
+VOID Routine_WriteFile(ADDRINT ip, ADDRINT sp)
+{
+    ADDRINT value[6];
+    //CHAR lpSubKey[50];
+    const CHAR* args[] = { "retAddress", "hFile", "lpBuffer", "nNumberOfBytesToWrite", "lpNumberOfBytesWritten", "lpOverlapped" };
+    std::string rtnName = RTN_FindNameByAddress(ip);
+    //Zero Memory
+    memset(value, 0, sizeof(value));
+    //memset(lpSubKey, 0, sizeof(lpSubKey));
+    //Read value pointed by SP
+    PIN_SafeCopy(value, (VOID*)sp, sizeof(value));
+    //PIN_SafeCopy(lpSubKey, (VOID*)value[2], sizeof(lpSubKey) - 1);
+    if (!rtnName.empty())
+    {
+        fprintf(traceF, "ip=%p, rtn_name=%s, sp=%p\n", ip, rtnName.c_str(), sp);
+        for (int i = 0; i < 6; i++)
+        {
+            fprintf(traceF, "  sp[%d]=%p (%s)\n", i, value[i], args[i]);
+        }
+    }
+}
+
+VOID Routine_Unknown(ADDRINT ip, ADDRINT sp)
+{
+    ADDRINT value[1];
+    //CHAR lpSubKey[50];
+    const CHAR* args[] = { "retAddress" };
+    std::string rtnName = RTN_FindNameByAddress(ip);
+    
+    //Zero Memory
+    memset(value, 0, sizeof(value));
+    //memset(lpSubKey, 0, sizeof(lpSubKey));
+    //Read value pointed by SP
+    PIN_SafeCopy(value, (VOID*)sp, sizeof(value));
+
+    PIN_LockClient();
+    IMG img = IMG_FindByAddress(value[0]);
+    std::string imgName;
+    size_t exist = -1;
+    if (IMG_Valid(img))
+    {
+        exist = IMG_Name(img).find("C:\\Windows\\SysWOW64");
+        imgName = IMG_Name(img);
+    }
+    PIN_UnlockClient();
+    if (exist == 0)
+    {
+        return;
+    }
+
+    //PIN_SafeCopy(lpSubKey, (VOID*)value[2], sizeof(lpSubKey) - 1);
+    if (!rtnName.empty())
+    {
+        fprintf(traceF, "img=%s, ip=%p, rtn_name=%s, sp=%p\n", imgName.c_str(), ip, rtnName.c_str(), sp);
+        for (int i = 0; i < 1; i++)
+        {
+            fprintf(traceF, "  sp[%d]=%p (%s)\n", i, value[i], args[i]);
         }
     }
 }
@@ -92,6 +211,34 @@ VOID Routine(RTN rtn, VOID* v)
         {
             RTN_Open(rtn);
             RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)Routine_LoadLibraryA,
+                IARG_ADDRINT, address, IARG_REG_VALUE, REG_STACK_PTR, IARG_END);
+            RTN_Close(rtn);
+        }
+        else if (name == "lstrlenA")
+        {
+            RTN_Open(rtn);
+            RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)Routine_lstrlenA,
+                IARG_ADDRINT, address, IARG_REG_VALUE, REG_STACK_PTR, IARG_END);
+            RTN_Close(rtn);
+        }
+        else if (name == "RegOpenKeyExA")
+        {
+            RTN_Open(rtn);
+            RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)Routine_RegOpenKeyExA,
+                IARG_ADDRINT, address, IARG_REG_VALUE, REG_STACK_PTR, IARG_END);
+            RTN_Close(rtn);
+        }
+        else if (name == "WriteFile")
+        {
+            RTN_Open(rtn);
+            RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)Routine_WriteFile,
+                IARG_ADDRINT, address, IARG_REG_VALUE, REG_STACK_PTR, IARG_END);
+            RTN_Close(rtn);
+        }
+        else
+        {
+            RTN_Open(rtn);
+            RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)Routine_Unknown,
                 IARG_ADDRINT, address, IARG_REG_VALUE, REG_STACK_PTR, IARG_END);
             RTN_Close(rtn);
         }
